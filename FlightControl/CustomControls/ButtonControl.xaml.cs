0978a37f50d9e2be0481291b97087efa079ea324
idrinkandmakethings <bartender@idrinkandmakethings.com>
2019-04-05 12:07:05 -0700

    Add button control

---
+++ FlightControl/CustomControls/ButtonControl.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace FlightControl.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for ButtonControl.xaml
+    /// </summary>
+    public partial class ButtonControl : UserControl
+    {
+        public ButtonControl()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectedColorChanged(object sender, System.Windows.RoutedPropertyChangedEventArgs<System.Windows.Media.Color?> e)
+        {
+            if (e.NewValue.HasValue)
+            {
+                ButtonDisplay.Fill = new SolidColorBrush(e.NewValue.Value);
+            }
+        }
+    }
+}
